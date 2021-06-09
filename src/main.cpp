6c0de9d477b875eeb7891ed6f89c8abef70e2482
Diego Rosario~ <diegorosario2013@gmail.com~>
2021-06-08 19:55:03 -0500

    possible link errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <centurion.hpp>
 
-int main() {
+int main(int, char**){
+    cen::library centurion;
+
+    // SDL2, SDL2_image, SDL2_mixer and SDL2_ttf are now initialized!
+
     return 0;
 }
