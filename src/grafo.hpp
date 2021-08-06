2413e50fa15186d04175fd83eee20dd4e50b77cb
Diego Rosario~ <diegorosario2013@gmail.com~>
2021-08-06 12:46:44 -0500

    refactoring propiedad

--- src/grafo.hpp
+++ src/grafo.hpp
@@ -3,30 +3,9 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include "propiedad.hpp"
+
 using namespace std;
-
-struct Propiedad {
-  string Nombre;
-  enum class Tipo { vivienda, comercial, trabajo };
-
-  int residentes = 0;
-
-  int Apertura;
-  int Cierre;
-
-  // https://www.geeksforgeeks.org/how-to-create-an-unordered_map-of-user-defined-class-in-cpp/
-  bool operator==(const Propiedad &p) const { return this->Nombre == p.Nombre; }
-};
-
-namespace std {
-
-template <> struct hash<Propiedad> {
-  size_t operator()(const Propiedad &x) const {
-    return hash<string>{}(x.Nombre);
-  }
-};
-// https://stackoverflow.com/questions/8157937/how-to-specialize-stdhashkeyoperator-for-user-defined-type-in-unordered
-} // namespace std
 
 using Arista = pair<Propiedad, int>;
 
